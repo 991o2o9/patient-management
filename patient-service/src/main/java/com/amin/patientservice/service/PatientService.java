eca2d1ec97754155bf217f9024eaea71597862b9
991o2o9 <1kkro123@gmail.com>
2026-05-04 00:07:29 +0600

    added grpc server connection between patient-service and billing-service

--- patient-service/src/main/java/com/amin/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/amin/patientservice/service/PatientService.java
@@ -4,6 +4,7 @@ import com.amin.patientservice.dto.PatientRequestDto;
 import com.amin.patientservice.dto.PatientResponseDto;
 import com.amin.patientservice.exception.EmailAlreadyExistsException;
 import com.amin.patientservice.exception.PatientNotFoundException;
+import com.amin.patientservice.grpc.BillingServiceGrpcClient;
 import com.amin.patientservice.mapper.PatientMapper;
 import com.amin.patientservice.model.Patient;
 import com.amin.patientservice.repository.PatientRepository;
@@ -16,21 +17,28 @@ import java.util.UUID;
 @Service
 public class PatientService {
     private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
     }
 
     public List<PatientResponseDto> getPatients() {
         List<Patient> patients = patientRepository.findAll();
         return patients.stream().map(PatientMapper::toDto).toList();
-    }       
+    }
 
     public PatientResponseDto createPatient(PatientRequestDto patientRequestDto) {
         if (patientRepository.existsByEmail(patientRequestDto.getEmail())) {
-            throw new EmailAlreadyExistsException("A ptient with this email already exist");
+            throw new EmailAlreadyExistsException("A patient with this email already exist");
         }
+
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDto));
+
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),
+                newPatient.getName(), newPatient.getEmail());
+
         return PatientMapper.toDto(newPatient);
     }
 
